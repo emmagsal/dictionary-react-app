7fa90f1a71547820e6ed7f6ad5dccfb830811e90
Emma <emmagsalisbury@gmail.com>
2023-03-28 10:42:45 +0100

    Starting layout

---
+++ src/Dictionary.js
@@ -0,0 +1,11 @@
+import React from "react";
+import './Dictionary.css';
+
+export default function Dictionary() {
+  return (
+      <div className="UserProfile">
+        <div>Hello</div>  
+        <div>World</div>
+      </div>
+  );
+}
