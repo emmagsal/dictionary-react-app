8f747f55ac5d95a962b42df5f8d705f06832c3e0
Emma <emmagsalisbury@gmail.com>
2023-03-28 10:49:51 +0100

    Added Dictionary into the correct place

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 
 import './App.css';
+import Dictionary from './Dictionary';
 
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Dictionary App
-      </header>
-      
+      <div className="container">
+        <header className="App-header">Dictionary App
+        </header>
+        <Dictionary />
+        <footer>Coded by Emma Salisbury</footer>
+      </div>
     </div>
   );
 }
