16eb228acecdcb55855b596760b5847160bdf60a
Emma <emmagsalisbury@gmail.com>
2023-04-08 19:34:50 +0100

    Images showing
    Grid doesnt seem to be responding

--- src/Photos.js
+++ src/Photos.js
@@ -1,10 +1,22 @@
 import React from "react";
 import axios from "axios";
+import "./Photos.css";
 
 export default function Photos(props) {
     if (props.photos) {
-        console.log(props.photos);
-        return "hello from Photos";
+        return (
+            <section className="Photos">
+                <div className="grid">
+                {props.photos.map(function (photo, index) {
+                    return (
+                        <div key={index}>
+                    <img src={photo.src.landscape} className="img-fluid" />
+                    </div>
+                    );
+                })}
+                </div>
+            </section>
+        )
     } else {
         return null;
     }
